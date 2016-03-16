6d5eae8c65c48f762a58ab209a269d014c13019e
sorasakUoU <nightontherock@gmail.com>
2016-03-16 11:20:20 +0700

    Post Data to mySQL Finish

--- app/src/main/java/kawinpart/sorasak/bicbook/SignUpActivity.java
+++ app/src/main/java/kawinpart/sorasak/bicbook/SignUpActivity.java
@@ -2,12 +2,22 @@ package kawinpart.sorasak.bicbook;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -40,7 +50,7 @@ public class SignUpActivity extends AppCompatActivity {
     public void clickSignUpSign(View view) {
 
         idCardString = idCardEditText.getText().toString().trim();
-        passwordString = idCardEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
 
         //Check Space
         if (idCardString.equals("") || passwordString.equals("")) {
@@ -94,6 +104,29 @@ public class SignUpActivity extends AppCompatActivity {
     } // confirmData
 
     private void updataDataToServer() {
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        try {
+
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("isAdd", "true"));
+            nameValuePairs.add(new BasicNameValuePair("ID_Card", idCardString));
+            nameValuePairs.add(new BasicNameValuePair("Password", passwordString));
+
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost("http://swiftcodingthai.com/bic/php_add_user_master.php");
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+            httpClient.execute(httpPost);
+
+
+
+            myToast("อัพเดทข้อมูล บน Server สำเร็จ");
+            finish();
+        } catch (Exception e){
+            myToast("ไม่สามารถเชื่อม Server ได้");
+        }
+
 
     } // updataToServer
 
