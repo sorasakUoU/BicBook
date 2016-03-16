826bd5a238758ffb4777c8ecb88cd30967465e94
sorasakUoU <nightontherock@gmail.com>
2016-03-16 12:05:53 +0700

    Finish 16-03-2016

--- app/src/main/java/kawinpart/sorasak/bicbook/MainActivity.java
+++ app/src/main/java/kawinpart/sorasak/bicbook/MainActivity.java
@@ -4,14 +4,50 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText idCardEditText, passwordEditText;
+    private String idCardString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        BindWidget();
+
+
+
     } //Main Method
+
+    private void BindWidget() {
+        idCardEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+
+    }
+
+    public void clickSingInMain(View view) {
+        idCardString = idCardEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (idCardString.equals("") || passwordString.equals("")) {
+            //Have Space
+            myAlert("โปรดกรอกให้ ครบทุกช่อง ค่ะ");
+        }
+
+
+
+    } // clickSignInMain
+
+    private void myAlert(String strAlert) {
+        Toast.makeText(MainActivity.this, strAlert, Toast.LENGTH_SHORT).show();
+    }
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
